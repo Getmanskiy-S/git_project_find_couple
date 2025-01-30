f54106b982f03ad52fec2f56668c3b0e75afdbed
Getmamskiy <seva-get@bk.ru>
2025-01-30 22:28:28 +0300

    Третий коммит. Список изменений: - изменена папка data - добавлена возможность играть с двумя парами карт - добавлена база данных (частич. функционал)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import sqlite3
 import pygame
-from random import randrange
+from random import randrange, choices, shuffle
+from math import ceil, sqrt
 
 
-def load_image(name):
-    fullname = os.path.join('data', name)
+def load_image(file_name):
+    fullname = os.path.join('data', file_name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -33,6 +35,21 @@ class Menu(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
+
+
+class TrueOrFalse(pygame.sprite.Sprite):
+    def __init__(self, group, cl_flag=False, cl_x=0, cl_y=0):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно !!!
+        super().__init__(group)
+        self.cl_flag = cl_flag
+        if self.cl_flag:
+            self.image = load_image('right.png')
+        else:
+            self.image = load_image('wrong.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = cl_x
+        self.rect.y = cl_y
 
 
 class Button(pygame.sprite.Sprite):
@@ -89,21 +106,32 @@ class MobileText(pygame.sprite.Sprite):
 
 class Card(pygame.sprite.Sprite):
 
-    def __init__(self, *group, image, cl_x=0, cl_y=0, cl_weight=800, cl_height=200):
+    def __init__(self, *group, file_path, cl_x=0, cl_y=0, cl_weight=200, cl_height=200):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно!!!
         super().__init__(*group)
-        self.image = load_image(image)
+        self.file_path = 'card_empty.png'
+        self.image = load_image(self.file_path)
+        self.image_card = file_path
         self.image = pygame.transform.scale(self.image, (cl_weight, cl_height))
         self.rect = self.image.get_rect()
         self.rect.x = cl_x
         self.rect.y = cl_y
 
     def update(self, *args):
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
-            pass
-        else:
-            pass
+        if (args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos) and
+                self.file_path == 'card_empty.png' and len(couple) < 2):
+            self.file_path = self.image_card
+            self.image = load_image(self.file_path)
+            couple.append(self)
+            if len(couple) == 2:
+                pygame.time.set_timer(pygame.USEREVENT, 3000)
+                if couple[0].file_path == couple[1].file_path:
+                    TrueOrFalse(temporary_sprites, True, couple[0].rect.x, couple[0].rect.y)
+                    TrueOrFalse(temporary_sprites, True, couple[1].rect.x, couple[1].rect.y)
+                else:
+                    TrueOrFalse(temporary_sprites, False, couple[0].rect.x, couple[0].rect.y)
+                    TrueOrFalse(temporary_sprites, False, couple[1].rect.x, couple[1].rect.y)
 
 
 def start_screen():
@@ -133,8 +161,8 @@ def start_screen():
                             return  # начинаем игру
                         elif i == 1:
                             for ss in text_sprites:
-                                ss.x = randrange(0, 1000)
-                                ss.y = randrange(0, 800)
+                                ss.x = randrange(1000)
+                                ss.y = randrange(800)
                         elif i == 2:
                             pass
                         else:
@@ -148,6 +176,15 @@ def start_screen():
         clock.tick(fps)
 
 
+def load_level():
+    menu.image = load_image(f'backgrounds/background{randrange(10)}.png')
+    cards_radom = choices(range(50), k=col_cards // 2)
+    cards_radom *= 2
+    shuffle(cards_radom)
+    for i in range(col_cards):
+        Card(cards_sprites, file_path=f'cards/card{cards_radom[i]}.png', cl_x=i * 200)
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 1000, 800
@@ -158,22 +195,50 @@ if __name__ == '__main__':
     button_sprites = pygame.sprite.Group()
     cards_sprites = pygame.sprite.Group()
     text_sprites = pygame.sprite.Group()
+    temporary_sprites = pygame.sprite.Group()
     clock = pygame.time.Clock()
+    player_id = 0
     start_screen()
 
-    menu.image = load_image(f'backgrounds/background{randrange(10)}.png')
+    # Подключение к БД
+    con = sqlite3.connect("users.db")
+    cur = con.cursor()
+    name, col_victory, col_defeat = cur.execute("""SELECT name, col_victory, col_defeat
+     FROM users WHERE id = ?""", (player_id,)).fetchone()
+    score, col_hint, col_cards = cur.execute("""SELECT score, col_hint, col_cards FROM saves 
+    WHERE id = ?""", (player_id,)).fetchone()
+
+    # введение переменных
+    couple = []  # пара карточек
+
+    # цикл уровня
+    load_level()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
+                cards_sprites.update(event)
                 x, y = event.pos
+            if event.type == pygame.USEREVENT and couple:
+                for s in temporary_sprites:
+                    if s.cl_flag:
+                        couple[0].kill()
+                        couple[1].kill()
+                        break
+                temporary_sprites.empty()
+                couple[0].file_path = 'card_empty.png'
+                couple[0].image = load_image(couple[0].file_path)
+                couple[1].file_path = 'card_empty.png'
+                couple[1].image = load_image(couple[1].file_path)
+                couple = []
         screen.blit(menu.image, menu.rect)
         button_sprites.draw(screen)
         text_sprites.update()
         cards_sprites.draw(screen)
-        cards_sprites.update()
+        temporary_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(fps)
     pygame.quit()
+    con.close()
